d5f121ba26bfe660bd866ecd0235734069a5f4c1
Listen <listenzz@163.com>
2018-02-09 10:55:15 +0800

    add splash

---
+++ demo/src/main/java/com/navigation/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.navigation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Listen on 2018/2/9.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        }, 500);
+    }
+
+}
