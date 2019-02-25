d6d2f08fa14b08afbfbfc91eeb57d7e22aac0022
Listen <listenzz@163.com>
2019-02-25 11:25:52 +0800

    add `NonNull` annotation

--- navigation/src/main/java/me/listenzz/navigation/FragmentHelper.java
+++ navigation/src/main/java/me/listenzz/navigation/FragmentHelper.java
@@ -20,7 +20,7 @@ public class FragmentHelper {
     private static final String TAG = "Navigation";
 
     @NonNull
-    public static Bundle getArguments(Fragment fragment) {
+    public static Bundle getArguments(@NonNull Fragment fragment) {
         Bundle args = fragment.getArguments();
         if (args == null) {
             args = new Bundle();
@@ -29,7 +29,7 @@ public class FragmentHelper {
         return args;
     }
 
-    public static void executePendingTransactionsSafe(FragmentManager fragmentManager) {
+    public static void executePendingTransactionsSafe(@NonNull FragmentManager fragmentManager) {
         try {
             fragmentManager.executePendingTransactions();
         } catch (IllegalStateException e) {
@@ -37,7 +37,10 @@ public class FragmentHelper {
         }
     }
 
-    public static void addFragmentToBackStack(FragmentManager fragmentManager, int containerId, AwesomeFragment fragment, PresentAnimation animation) {
+    public static void addFragmentToBackStack(@NonNull FragmentManager fragmentManager, int containerId, @NonNull AwesomeFragment fragment, @NonNull PresentAnimation animation) {
+        if (fragmentManager.isDestroyed()) {
+            return;
+        }
         executePendingTransactionsSafe(fragmentManager);
 
         FragmentTransaction transaction = fragmentManager.beginTransaction();
@@ -54,11 +57,14 @@ public class FragmentHelper {
         transaction.commit();
     }
 
-    public static void addFragmentToAddedList(FragmentManager fragmentManager, int containerId, AwesomeFragment fragment) {
+    public static void addFragmentToAddedList(@NonNull FragmentManager fragmentManager, int containerId, @NonNull AwesomeFragment fragment) {
         addFragmentToAddedList(fragmentManager, containerId, fragment, true);
     }
 
-    public static void addFragmentToAddedList(FragmentManager fragmentManager, int containerId, AwesomeFragment fragment, boolean primary) {
+    public static void addFragmentToAddedList(@NonNull FragmentManager fragmentManager, int containerId, @NonNull AwesomeFragment fragment, boolean primary) {
+        if (fragmentManager.isDestroyed()) {
+            return;
+        }
         executePendingTransactionsSafe(fragmentManager);
 
         FragmentTransaction transaction = fragmentManager.beginTransaction();
@@ -70,7 +76,7 @@ public class FragmentHelper {
     }
 
     @Nullable
-    public static AwesomeFragment getLatterFragment(FragmentManager fragmentManager, AwesomeFragment fragment) {
+    public static AwesomeFragment getLatterFragment(@NonNull FragmentManager fragmentManager, @NonNull AwesomeFragment fragment) {
         int count = fragmentManager.getBackStackEntryCount();
         int index = findIndexAtBackStack(fragmentManager, fragment);
         if (index > -1 && index < count - 1) {
@@ -81,7 +87,7 @@ public class FragmentHelper {
     }
 
     @Nullable
-    public static AwesomeFragment getAheadFragment(FragmentManager fragmentManager, AwesomeFragment fragment) {
+    public static AwesomeFragment getAheadFragment(@NonNull FragmentManager fragmentManager, @NonNull AwesomeFragment fragment) {
         int count = fragmentManager.getBackStackEntryCount();
         int index = findIndexAtBackStack(fragmentManager, fragment);
         if (index > 0 && index < count) {
@@ -91,7 +97,7 @@ public class FragmentHelper {
         return null;
     }
 
-    public static int findIndexAtBackStack(FragmentManager fragmentManager, AwesomeFragment fragment) {
+    public static int findIndexAtBackStack(@NonNull FragmentManager fragmentManager, @NonNull AwesomeFragment fragment) {
         int count = fragmentManager.getBackStackEntryCount();
         int index = -1;
         for (int i = 0; i < count; i++) {
