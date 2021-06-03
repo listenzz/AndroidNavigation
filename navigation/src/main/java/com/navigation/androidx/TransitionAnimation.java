9a76469a80092797cf4af227dcf88b3ed503ec16
listen <listenzz@163.com>
2021-06-03 22:38:38 +0800

    优化重定向的转场动画

--- navigation/src/main/java/com/navigation/androidx/TransitionAnimation.java
+++ navigation/src/main/java/com/navigation/androidx/TransitionAnimation.java
@@ -9,7 +9,7 @@ import androidx.annotation.AnimRes;
 public class TransitionAnimation {
 
     public final static TransitionAnimation Push = new TransitionAnimation(R.anim.nav_push_enter, R.anim.nav_push_exit, R.anim.nav_pop_enter, R.anim.nav_pop_exit);
-    public final static TransitionAnimation Redirect = new TransitionAnimation(R.anim.nav_push_enter,  R.anim.nav_push_exit,  R.anim.nav_push_enter,  R.anim.nav_push_exit);
+    public final static TransitionAnimation Redirect = new TransitionAnimation(R.anim.nav_push_enter,  R.anim.nav_gone,  R.anim.nav_gone,  R.anim.nav_push_exit);
     public final static TransitionAnimation Present = new TransitionAnimation(R.anim.nav_present_enter, R.anim.nav_present_exit, R.anim.nav_dismiss_enter, R.anim.nav_dismiss_exit);
     public final static TransitionAnimation Fade = new TransitionAnimation(R.anim.nav_fade_in, R.anim.nav_fade_out, R.anim.nav_fade_in, R.anim.nav_fade_out);
     public final static TransitionAnimation None = new TransitionAnimation(R.anim.nav_none, R.anim.nav_none, R.anim.nav_none, R.anim.nav_none);
