e41ff3599904a490f62d4f18a74489d09f17b716
listen <listenzz@163.com>
2024-01-29 17:20:08 +0800

    更新 demo, 处理 Android 12 的闪屏实现

--- demo/src/main/java/com/navigation/SplashActivity.java
+++ demo/src/main/java/com/navigation/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.navigation;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -13,6 +14,14 @@ public class SplashActivity extends AwesomeActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            getSplashScreen().setOnExitAnimationListener(splashScreenView -> {
+                splashScreenView.remove();
+                getSplashScreen().clearOnExitAnimationListener();
+            });
+        }
+
         new Handler().postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, MainActivity.class));
             overridePendingTransition(R.anim.nav_fade_in, R.anim.nav_fade_out);
